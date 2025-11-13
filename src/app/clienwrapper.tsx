d25c10cb7c49660611636723725fdc3c6456596f
Arthur <hitsuga2000@gmail.com>
2025-11-13 03:06:25 +0100

    fix: bugs

--- src/app/clienwrapper.tsx
+++ src/app/clienwrapper.tsx
@@ -1,7 +1,6 @@
 "use client";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { queryClient } from "@/queryClient";
-// @ts-expect-error: React Query Devtools missing type declarations
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 export default function ClientProviderWrapper({
